f7986f7415cb08179391070fc62fafd14867ac34
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-04-25 15:34:55 +0900

    initial-set

---
+++ src/main/java/networklab/smartapp/SmartappApplication.java
@@ -0,0 +1,13 @@
+package networklab.smartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/networklab/smartapp/SmartappApplicationTests.java
@@ -0,0 +1,13 @@
+package networklab.smartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
