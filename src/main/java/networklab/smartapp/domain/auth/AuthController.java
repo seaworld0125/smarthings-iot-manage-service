f2c67bfe77043bb83c9f6cd8cbb7393266676d2c
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-07-08 00:25:55 +0900

    Update AuthController.java

--- src/main/java/networklab/smartapp/domain/auth/AuthController.java
+++ src/main/java/networklab/smartapp/domain/auth/AuthController.java
@@ -1,27 +1,42 @@
 package networklab.smartapp.domain.auth;
 
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import networklab.smartapp.error.exception.BusinessException;
-import networklab.smartapp.error.exception.ErrorCode;
+import networklab.smartapp.domain.dto.AuthDto;
+import networklab.smartapp.domain.dto.JoinDto;
+import networklab.smartapp.domain.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping(value = "auth")
 public class AuthController {
+
     private final AuthService authService;
 
-    @PostMapping
-    public ResponseEntity auth(@RequestBody @Valid AuthDto authDto) {
-        boolean result = authService.login(authDto.getPassword());
+    @GetMapping
+    public String getAuthPage() {
+        return "auth";
+    }
 
-        if(!result) throw new BusinessException(ErrorCode.PASSWORD_INVALID);
-        return new ResponseEntity(HttpStatus.OK);
+    @PostMapping
+    public ResponseEntity<ResponseDto> auth(AuthDto authDto) {
+
+        authService.authVerify(authDto);
+        return new ResponseEntity<>(ResponseDto.of("로그인 성공"), HttpStatus.OK);
+    }
+
+    @GetMapping("/join")
+    public String getJoinPage() { return "join"; }
+
+    @PostMapping("/join")
+    public ResponseEntity<ResponseDto> join(JoinDto joinDto) {
+
+        authService.joinVerify(joinDto);
+        return new ResponseEntity<>(ResponseDto.of("회원가입 성공"), HttpStatus.OK);
     }
 }
